2918124fae0744a3699ae9528888b24626159b5b
Brianprice098 <103675908+Brianprice098@users.noreply.github.com>
2022-04-20 14:11:27 -0400

    "First mortgage calculator"

--- src/main/java/strings.java
+++ src/main/java/strings.java
@@ -1,8 +1,33 @@
+import java.awt.*;
+import java.text.NumberFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class strings {
-    public static void main(String[] args){
-        byte age = 30;
-        Date now = new Date();
-        System.out.println(now);
-}}
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR =12;
+        final byte PERCENT=100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual interest rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.print("Period (Years): ");
+        byte years =scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                *(monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                /(Math.pow(1 + monthlyInterest, numberOfPayments)-1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(1);
+        System.out.println("Mortgage: " + mortgageFormatted);
+
+
+
+    }}
